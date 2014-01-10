e62b4d12001671190ff137e4745854c9fd440fa2
Tarvitz <tarvitz@blacklibrary.ru>
2014-01-10 04:08:47 +0400

    add default font and watermark script

--- tools/watermark.py
+++ tools/watermark.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python
 import os
 import sys
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
+import logging as log
+log.basicConfig(level=log.INFO)
 
 
 def usage():
-	print("%s <file.jpg|png|gif" % sys.argv[0])
-	sys.exit(-1)
+    print("%s <file.jpg|png|gif> message" % sys.argv[0])
+    sys.exit(-1)
 
 def main(text):
-	image = Image.open(sys.argv[1])
-	mark = Image.new('RGBA', image.size)
-	mark = ImageDraw.ImageDraw(mark, "RGBA")
-	mark.text((0, 100), text)
-	mark = mark.convert("L").point(lambda x: min(x, 100))
-	mark.putalpha(mark)
-	image.paste(mark, None, mark)
-	image.save(sys.argv[1], "JPEG")
+    font = ImageFont.truetype("../fonts/DejaVuSans.ttf", 24)
+    image = Image.open(sys.argv[1])
+    draw = ImageDraw.Draw(image)
+    draw.text((0, 50), text, (255, 255, 255), font=font)
+    log.info("saving file")
+    image.save(sys.argv[1], "JPEG")
 
 if __name__ == '__main__':
-	main("test out")
+    if len(sys.argv) < 3:
+        usage()
+    main(sys.argv[2])
