ab38333d071c4d86aa8d137c1014445e7a647e3e
Tarvitz <tarvitz@blacklibrary.ru>
2014-02-05 22:52:26 +0400

    update Makefile

--- Makefile
+++ Makefile
@@ -1,15 +1,18 @@
-SRC="docs/steamos"
-CSS_INCLUDE="css/bootstrap.min.css"
-CSS_INCLUDE_PDF="bootstrap.min.css,pdf.css"
-CSS_PATH='css'
+MODULES = docs/steamos.rst docs/streaming.rst
+HTML_MODULES = $(MODULES:.rst=.html)
+FLAGS="--stylesheet=css/bootstrap.min.css"
+RST2HTML=rst2html.py
+TOOL_CHAIN=tools/js_attachments.py
 
 all:
-	@echo "targets: html pdf"
-html:
-	rst2html.py --stylesheet=${CSS_INCLUDE} ${SRC}.rst > ${SRC}.html
-	tools/js_attachments.py ${SRC}.html
-pdf:
-	rst2pdf --stylesheets=${CSS_INCLUDE_PDF} --stylesheet-path=${CSS_PATH} \
-	-l ru_RU ${SRC}.rst > ${SRC}.pdf
+	@echo "targets: html"
+
+%.html: %.rst
+	$(RST2HTML) ${FLAGS} $< > $@
+	${TOOL_CHAIN} $@
+
 clean:
-	rm -f *.html
+	rm -f ${HTML_MODULES}
+
+html: ${HTML_MODULES}
+	@echo "done"
--- tools/js_attachments.py
+++ tools/js_attachments.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 """
-boosts html version with js/css extensions:
-* colorbox
+boosts html version with js/css extensions::
+
+    * colorbox
+
 """
+
 import re
 import sys
 
@@ -14,37 +17,42 @@ JS_INJECTION = """
 <script type='text/javascript' src='../js/colorbox/jquery.colorbox-min.js'></script>
 
 <script type='text/javascript'>
-	$(document).ready(function(){
-		$(".image-reference").colorbox({rel: ".image-reference"});
-	});
+    $(document).ready(function(){
+        $(".image-reference").colorbox({rel: ".image-reference"});
+    });
 </script>
 """
 
+
 def usage():
-	print("%s <file.html>" % sys.argv[0])
-	sys.exit(-1)
+    print("%s <file.html> [file2.html file3.html .. file_n.html]"
+          % sys.argv[0])
+    sys.exit(-1)
+
 
 def insert_into(base, substr, index=0):
-	"""
-	inserts substr into base string with given index
-	
-	>>> insert_into("test", "me ")
-	'me test'
-	>>> insert_into("test", "me ", index=3)
-	'tesme t'
-	"""
+    """
+    inserts substr into base string with given index
 
-	return base[:index] + substr + base[index:]
+    >>> insert_into("test", "me ")
+    'me test'
+    >>> insert_into("test", "me ", index=3)
+    'tesme t'
+    """
+
+    return base[:index] + substr + base[index:]
+
 
 def main():
-	if len(sys.argv) < 2:
-		usage()
-	document = open(sys.argv[1]).read()
-	idx = document.index('</body>')
-	document = insert_into(document, JS_INJECTION, idx)
-	idx = document.index('</head>')
-	document = insert_into(document, CSS_INJECTION, idx)
-	open(sys.argv[1], 'w').write(document)
+    if len(sys.argv) < 2:
+        usage()
+    for i in xrange(1, len(sys.argv)):
+        document = open(sys.argv[i]).read()
+        idx = document.index('</body>')
+        document = insert_into(document, JS_INJECTION, idx)
+        idx = document.index('</head>')
+        document = insert_into(document, CSS_INJECTION, idx)
+        open(sys.argv[i], 'w').write(document)
 
 if __name__ == '__main__':
-	main()
+    main()
